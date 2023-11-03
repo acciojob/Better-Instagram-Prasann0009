17e2314ab996d3896a63da21dfec47f3dc085326
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-11-03 15:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your code here
+const images = document.querySelectorAll(".image");
+
+for(let i=0;i<images.length;i++)
+{
+   images[i].addEventListener("dragstart",onDragStart);
+   images[i].addEventListener("dragover",onDragOver);
+   images[i].addEventListener("drop",onDrop);
+}
+  let dragImage;
+  let dragOverImage;
+
+
+function onDragStart(event)
+{
+  dragImage = event.target;
+}
+function onDragOver(event)
+{
+  dragOverImage = event.target;
+  event.preventDefault();
+}
+function onDrop(event)
+{
+  const dropImage = event.target;
+  const ImageDrag = window.getComputedStyle(dragImage).backgroundImage;
+  const ImageDragId = dragImage.id;
+  const ImageDragText = dragImage.innerText;
+   
+  const dropImageId = dropImage.id;
+  const dropImageText= dropImage.innerText;
+  const dropImageName = window.getComputedStyle(dropImage).backgroundImage;
+
+  dropImage.setAttribute("id",ImageDragId);
+  dropImage.innerText = ImageDragText;
+dropImage.style.backgroundImage = ImageDrag;
+
+dragImage.setAttribute("id",dropImageId);
+  dragImage.innerText = dropImageText;
+dragImage.style.backgroundImage = dropImageName;
+
+}
